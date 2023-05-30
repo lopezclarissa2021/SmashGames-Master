7c05d1aa2ed0841070f3820a4a077d34f04c6571
Clarissa Lopez <102737214+lopezclarissa2021@users.noreply.github.com>
2023-05-30 11:55:53 -0700

    added data to script

---
+++ script.js
@@ -0,0 +1,27 @@
+// index.html Smash Games - Home
+// blast.html Smash Games - Inferno Blast
+
+const data = {
+  brandName: "Smash Games",
+  pages: [
+    {
+      name: "index",
+      pageName: "Home",
+      callToAction: {
+        src: "images/SmashGamesLogo-short.png",
+        altText: "Smash Games logo"
+      }
+    },
+    {
+      name: "blast",
+      pageName: "Inferno Blast",
+      callToAction: {
+        src: "images/Inferno-Jumbotron.png",
+        altText: "Inferno Blast Gameplay",
+        buttonLinkSrc: "https://steampowered.com",
+        buttonLinkText: "Buy now on Steam"
+      }
+    }
+  ]
+
+}
